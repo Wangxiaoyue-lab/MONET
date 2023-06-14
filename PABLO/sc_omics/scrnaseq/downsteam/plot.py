d6910af5666dd9f2e77b40b74361c82a89bd450d
Moloch0 <luoliheng@nwpu.mail.edu.cn>
2023-06-14 22:27:59 +0800

    new file:   PABLO.egg-info/PKG-INFO

---
+++ PABLO/sc_omics/scrnaseq/downsteam/plot.py
@@ -0,0 +1,16 @@
+def split_umap(adata, split_by, ncol=2, nrow=None, **kwargs):
+    import scanpy as sc
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    categories = adata.obs[split_by].cat.categories
+    if nrow is None:
+        nrow = int(np.ceil(len(categories) / ncol))
+    fig, axs = plt.subplots(nrow, ncol, figsize=(5 * ncol, 4 * nrow))
+    axs = axs.flatten()
+    for i, cat in enumerate(categories):
+        ax = axs[i]
+        sc.pl.umap(
+            adata[adata.obs[split_by] == cat], ax=ax, show=False, title=cat, **kwargs
+        )
+    plt.tight_layout()
---
+++ PABLO/sc_omics/scrnaseq/downsteam/preprocess.py
@@ -0,0 +1,39 @@
+def run_sctransform(adata, layer=None, **kwargs):
+    # Extract the representation matrix layer from the anndata object
+    import anndata2ri
+    from rpy2.robjects.packages import importr
+    from rpy2.robjects import r, pandas2ri
+    import numpy as np
+
+    anndata2ri.activate()
+    pandas2ri.activate()
+    if layer:
+        mat = adata.layers[layer]
+    else:
+        mat = adata.X
+
+    # Set the column names of the input matrix
+    cell_names = adata.obs_names
+    gene_names = adata.var_names
+    r.assign("mat", mat.T)
+    r.assign("cell_names", cell_names)
+    r.assign("gene_names", gene_names)
+    r("colnames(mat) <- cell_names")
+    r("rownames(mat) <- gene_names")
+
+    # Convert the matrix into a Seurat object
+    seurat = importr("Seurat")
+    r("seurat_obj <- CreateSeuratObject(mat)")
+
+    # Run SCTransform
+    for k, v in kwargs.items():
+        r.assign(k, v)
+    kwargs_str = ", ".join([f"{k}={k}" for k in kwargs.keys()])
+    r(f'seurat_obj <- SCTransform(seurat_obj,vst.flavor="v2", {kwargs_str})')
+
+    # Extract the SCT data and add it as a new layer in the original anndata object
+    sct_data = np.asarray(r["as.matrix"](r("seurat_obj@assays$SCT@data")))
+    adata.layers["SCT_data"] = sct_data.T
+    sct_data = np.asarray(r["as.matrix"](r("seurat_obj@assays$SCT@counts")))
+    adata.layers["SCT_counts"] = sct_data.T
+    return adata
